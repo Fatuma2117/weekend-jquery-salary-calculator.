e0e11bce07e06e3e9cecd9416d8976858686721d
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-04-08 15:36:52 -0500

    monthlyTotal function displayed

--- salary.js
+++ salary.js
@@ -1,43 +1,63 @@
 $(document).ready(readyNow)
-
+let employee = []
 ////Psuedo Code!! 
 //input form should collect data 
-    // add information for submit button 
+// add information for submit button 
 // append inputs to DOM
 // store the input information 
 // clear input fields
 // calculate monthly costs
 // if monthly costs> 20000 return red background
 
-function readyNow(){
-$('#submitEmployee').on('click', addClickHandler)
+function readyNow() {
+    $('#submitEmployee').on('click', addNewEmployee)
 }
+function addNewEmployee() {
+    // console.log('working?') 
+    let inputs = {
+        firstInput: $("#firstNameInput").val(),
+        lastInput: $("#lastNameInput").val(),
+        idInput: $("#idInput").val(),
+        titleInput: $("#titleInput").val(),
+        annualInput: $("#annualInput").val()
 
-function addClickHandler() {
-console.log('working?')
-let firstInput = $("#firstNameInput").val();
-let lastInput = $("#lastNameInput").val();
-let idInput = $("#idInput").val();
-let titleInput = $("#titleInput").val();
-let annualInput = $("#annualInput").val();
+    }
+    employee.push(inputs)
+    // console.log(inputs.annualInput)
 
- $('#tableBody').append(`<tr>
-    <td> ${firstInput}</td>
-    <td>${lastInput} </td>
-    <td> ${idInput}</td>
-    <td> ${titleInput}</td>
-    <td> ${annualInput}</td>
+    $('#tableBody').append(`<tr>
+    <td> ${inputs.firstInput}</td>
+    <td>${inputs.lastInput} </td>
+    <td> ${inputs.idInput}</td>
+    <td> ${inputs.titleInput}</td>
+    <td> ${inputs.annualInput}</td>
     </tr>`);
-$("#firstNameInput").val('');
-$("#lastNameInput").val('');
-$("#idInput").val('');
-$("#titleInput").val('');
-$("#annualInput").val('');
+
+    $("#firstNameInput").val('');
+    $("#lastNameInput").val('');
+    $("#idInput").val('');
+    $("#titleInput").val('');
+    $("#annualInput").val('');
+
+    calculateTotalMonthly();
 
 }
 
-function totalCalculator(){
-// the total monthly costs of $("#annualInput").
+function calculateTotalMonthly() {
+    // the total monthly costs of $("#annualInput").
+    let monthlyTotal = 0;
+    // loop through addNewEmployee
+    for (let i = 0; i < employee.length; i++) {
+        //for each employee, add up total annual/12
+       monthlyTotal += Number(employee[i].annualInput/12)
+       if(monthlyTotal > 20000) { 
+        $('#totalMonthlyOut').style.color = 'red'
+       }
+    }
+// console.log(monthlyTotal)    // display calculateTotalMonthly
+let outPut = $('#totalMonthlyOut')
+outPut.empty();
+outPut.append(Number(monthlyTotal))
 
 
 }
