38f09b950944b7f6749fcba489430ad90c87b027
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-04-07 19:04:57 -0500

    html table

--- salary.js
+++ salary.js
@@ -1,5 +1,19 @@
 $(document).ready(readyNow)
 
+////Psuedo Code!! 
+//input form should collect data 
+    // add information for submit button 
+// append inputs to DOM
+// store the input information 
+// clear input fields
+// calculate monthly costs
+// if monthly costs> 20000 return red background
+
+
+
+
+
+
 function readyNow(){
 $('#submitEmployee').on('click', addClickHandler)
 }
@@ -22,8 +36,6 @@ $('#tableBody').append(`<tr>
 <td> ${titleInput}</td>
 <td> ${annualInput}</td>
 </tr>`);
-
-
 
 }
 
